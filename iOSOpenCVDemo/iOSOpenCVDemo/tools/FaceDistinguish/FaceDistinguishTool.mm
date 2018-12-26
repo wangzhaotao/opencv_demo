591d7ece4b4672c70a4a860441fe8047059cf1b2
wangzhaotao <tyler.wang>
2018-12-26 20:04:56 +0800

    changes

---
+++ iOSOpenCVDemo/iOSOpenCVDemo/tools/FaceDistinguish/FaceDistinguishTool.mm
@@ -0,0 +1,26 @@
+//
+//  FaceDistinguishTool.m
+//  iOSOpenCVDemo
+//
+//  Created by ocean on 2018/12/26.
+//  Copyright © 2018年 wztMac. All rights reserved.
+//
+
+#import <opencv2/stitching/detail/blenders.hpp>
+#import <opencv2/stitching/detail/exposure_compensate.hpp>
+
+#import "FaceDistinguishTool.h"
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+//#include <opencv2/face/facerec.hpp>
+//#include <opencv2/face.hpp>
+//矩阵帮助类
+#include <opencv2/highgui.hpp>
+#include <opencv2//core//types_c.h>
+#include <opencv2/core/core.hpp>
+
+@implementation FaceDistinguishTool
+
+@end
